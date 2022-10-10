4768dc4ead32b546995a07444914819f559632e6
Rodrigo Loredo <loredo.rod@gmail.com>
2022-10-10 10:42:43 +0200

    implement list command (#8)

--- git_jira/cli.py
+++ git_jira/cli.py
@@ -3,13 +3,14 @@ import click
 from git_jira.commands.branch import branch
 from git_jira.commands.configure import configure
 from git_jira.commands.status import status
+from git_jira.commands.list import list
 
 
 @click.group()
 def cli():
     pass
 
-
 cli.add_command(configure)
 cli.add_command(branch)
 cli.add_command(status)
+cli.add_command(list)
---
+++ git_jira/commands/list.py
@@ -0,0 +1,22 @@
+import click
+from git_jira.jira import JiraProject
+from git_jira.git import GitRepo
+from git_jira.utils import print_table
+
+columns = ['key', 'type', 'summary', 'branch', 'asignee']
+
+def classify_issues(project_code=None, status=None):
+    issues = JiraProject(project_code=project_code).get_issues(status=status)
+    branches = GitRepo().get_branches()
+    classified_issues = [[issue.key, issue.type, issue.summary, "no branch", issue.asignee] for issue in issues if issue.branch_name not in branches]
+    classified_issues.extend([[issue.key, issue.type, issue.summary, issue.branch_name, issue.asignee] for issue in issues if issue.branch_name in branches])
+    return classified_issues
+
+
+@click.command()
+@click.option('-s', '--status', help='List issues with this status', default='Planned', type=str)
+def list(status):
+    click.echo("Getting available issues...")
+    issues = classify_issues(status=status)
+    click.echo(print_table(columns, issues))
+    
