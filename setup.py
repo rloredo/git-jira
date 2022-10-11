1323ed045216605b8f997f41b8076320a5d35f28
rloredo <loredo.rod@gmail.com>
2022-10-11 18:11:22 +0200

    bump to 0.3.0 version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="A git addon to manage Jira from git",
     long_description=README,
     long_description_content_type="text/markdown",
-    version="0.2.1",
+    version="0.3.0",
     license="MIT",
     author="rloredo",
     author_email="loredo.rod@gmail.com",
